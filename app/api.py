ff72ef1ef40b9b35a6de32883a029adefd94c22a
Mani Alimadadi <mani.alimadadi7@gmail.com>
2025-07-15 16:57:08 +0330

    Add /get-file

--- app/api.py
+++ app/api.py
@@ -1,15 +1,17 @@
 from uuid import uuid4
 
-from fastapi import FastAPI, UploadFile
+from fastapi import FastAPI, Response, UploadFile
+from fastapi.responses import StreamingResponse
 
-from app.minio_client import MinioClient
+from app.minio_client import MinioClientDep
 
 app = FastAPI()
 
+BUCKET_NAME = "to-process"
+
 
 @app.post("/upload")
-async def upload_image(file: UploadFile):
-    minio_client = MinioClient()
+async def upload_image(file: UploadFile, minio_client: MinioClientDep):
 
     # TODO: check if file is a valid image
     # TODO: check empty file
@@ -20,6 +22,12 @@ async def upload_image(file: UploadFile):
     filename = file.filename or f"{uuid4()}.{extension}"
     size = file.size or 0
 
-    minio_client.upload_file("to-process", filename, file.file, size, content_type)
+    minio_client.upload_file(BUCKET_NAME, filename, file.file, size, content_type)
 
     return {"message": "Image uploaded successfully"}
+
+
+@app.get("/get-file")
+async def get_file(filename: str, minio_client: MinioClientDep):
+    file_data, content_type = minio_client.get_file(BUCKET_NAME, filename)
+    return Response(content=file_data, media_type=content_type)
