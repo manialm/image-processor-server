8ba5e08304ce84d6bdb098a4b43ef6fbaafb6a4b
Mani Alimadadi <mani.alimadadi7@gmail.com>
2025-07-18 04:54:49 +0330

    Format

--- app/minio_client.py
+++ app/minio_client.py
@@ -81,7 +81,6 @@ class MinioClient:
             logger.error(f"Error occurred during get file: {exc}")
             raise
 
-
     @retry(
         stop=stop_after_attempt(3), wait=wait_exponential(multiplier=1, min=4, max=15)
     )
--- testing.py
+++ testing.py
@@ -2,6 +2,8 @@ from tenacity import retry, stop_after_attempt, wait_exponential
 
 
 counter = 0
+
+
 @retry(stop=stop_after_attempt(2), wait=wait_exponential(multiplier=1, min=4, max=15))
 def test_function():
     raise Exception("test")
